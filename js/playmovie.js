90b33ecdede9ca735f9933f70a7f51002dd1736f
Mariela Insaurralde <minsaurralde@teco.com.ar>
2022-12-20 12:55:28 -0300

    adding spinner on playmovie.html

--- js/playmovie.js
+++ js/playmovie.js
@@ -14,12 +14,15 @@ const playMovie = () => {
   document.getElementById(
     "titulo"
   ).innerText = `${seleccion.title} ( ${seleccion.release_date} )`;
-
   document.getElementById("descripcion").innerText = seleccion.overview;
-
   document.getElementsByTagName(
     "iframe"
   )[0].src = `https://www.youtube.com/embed/${video.key}`;
+
+  setTimeout(() => {
+    document.getElementById("spinner").classList.replace("d-flex", "d-none");
+    document.getElementById("video").classList.replace("d-none", "d-block");
+  }, 1000);
 };
 
 ////////////////////////////////////////////////////////////////////////////
